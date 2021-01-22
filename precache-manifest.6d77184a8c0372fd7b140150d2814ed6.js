39c0916c1fb8b9484ff2013bc019dd00d5d24497
ac066849 <adil.c@cerner.com>
2021-01-22 15:19:17 +0530

    Updates

---
+++ precache-manifest.6d77184a8c0372fd7b140150d2814ed6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470acf8598f203c90eebf14e4ed87e9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffdd608d260ab5868c64",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "fb0b71fb40c2fdd6222f",
+    "url": "/static/css/main.58d9892f.chunk.css"
+  },
+  {
+    "revision": "ffdd608d260ab5868c64",
+    "url": "/static/js/2.b124d168.chunk.js"
+  },
+  {
+    "revision": "90e21a658676b08d853d767fe5b667e4",
+    "url": "/static/js/2.b124d168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0b71fb40c2fdd6222f",
+    "url": "/static/js/main.c481fc84.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "1fdc1108e89a67cd4444ae9c6a446ebd",
+    "url": "/static/media/Gtm.1fdc1108.jpg"
+  },
+  {
+    "revision": "db13f6cea817916abdcbd0e70590d367",
+    "url": "/static/media/adil_avtar.db13f6ce.jpeg"
+  },
+  {
+    "revision": "a83c0b477273076fbba186b4b68182ed",
+    "url": "/static/media/c1.a83c0b47.png"
+  },
+  {
+    "revision": "40fb9a098b8c9c23d98a84acf2fb8975",
+    "url": "/static/media/c2.40fb9a09.png"
+  },
+  {
+    "revision": "82b5d019f0c6fb6f72dc11ab8cb4cc18",
+    "url": "/static/media/c3.82b5d019.png"
+  },
+  {
+    "revision": "c1d6bfef08ae9f826969c9b2776ab684",
+    "url": "/static/media/ci-proj.c1d6bfef.jpg"
+  },
+  {
+    "revision": "d250d854279ba3202d23391e59d2996d",
+    "url": "/static/media/gst-table.d250d854.png"
+  },
+  {
+    "revision": "8820fcebd0543921ea15156dd88ec1f9",
+    "url": "/static/media/gst-table2.8820fceb.jpeg"
+  },
+  {
+    "revision": "5a748cc7d89e366381beb3a510236cf4",
+    "url": "/static/media/gst2.5a748cc7.png"
+  },
+  {
+    "revision": "732a9f5d79ac86775a46fcec6f172e83",
+    "url": "/static/media/jorney-builder.732a9f5d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c501581791d4955473e0fdb198514007",
+    "url": "/static/media/rails-img.c5015817.png"
+  }
+]);
